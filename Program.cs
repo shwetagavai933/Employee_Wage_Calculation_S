30faf04f0604ae6e9543cda7e7fda937e519a4b1
shwetagavai933 <shwetagavai933@gmail.com>
2021-09-05 16:13:47 +0530

    [Shweta]: Calculating Employee Wage Using Switch Case

--- Program.cs
+++ Program.cs
@@ -4,30 +4,29 @@ namespace EmployeeWageCalculations
 {
     class Employee
     {
+        public const int IS_PRESENT_FULL_TIME = 1;
+        public const int IS_PRESENT_HALF_TIME = 2;
+        public const int EMP_RATE_PER_HOUR = 20;
         static void Main(string[] args)
         {
-            int IS_PRESENT_FULL_TIME = 1;
-            int IS_PRESENT_HALF_TIME = 2;
-            int EMP_RATE_PER_HOUR = 20;
-
             int empHrs = 0;
             int empWage = 0;
             
             Random random = new Random();
             int empCheck = random.Next(0, 2);
-
-            if (empCheck == IS_PRESENT_FULL_TIME)
+            switch (empCheck)
             {
-                empHrs = 8;
+                case IS_PRESENT_FULL_TIME:
+                    empHrs = 8;
+                    break;
+                case IS_PRESENT_HALF_TIME:
+                    empHrs = 4;
+                    break;
+                default:
+                    empHrs = 0;
+                    break;
             }
-            else if (empCheck == IS_PRESENT_HALF_TIME)
-            {
-                empHrs = 4;
-            }
-            else 
-            {
-                empHrs = 0;
-            }
+            
             empWage = empHrs * EMP_RATE_PER_HOUR;
             Console.WriteLine("Daily Employee Wage is:" + empWage);
         }
