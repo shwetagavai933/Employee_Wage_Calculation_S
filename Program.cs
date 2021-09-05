61db21ad5d48f045a5cc1b271f150a04bf546767
shwetagavai933 <shwetagavai933@gmail.com>
2021-09-05 18:31:44 +0530

    [Shweta]: Calculating Employee Wage with Condition of Days and Hours

--- Program.cs
+++ Program.cs
@@ -7,15 +7,17 @@ namespace EmployeeWageCalculations
         public const int IS_PRESENT_FULL_TIME = 1;
         public const int IS_PRESENT_HALF_TIME = 2;
         public const int EMP_RATE_PER_HOUR = 20;
-        public const int WORK_DAYS_PER_MONTH = 10;
+        public const int WORK_DAYS_PER_MONTH = 20;
+        public const int MAX_HRS_IN_MONTH = 100;
         static void Main(string[] args)
         {
             int empHrs = 0;
-            int empWage = 0;
-            int totalEmpWage = 0;
+            int totalEmpHrs = 0;
+            int totalWorkingDays = 0;
 
-            for (int day = 0; day < WORK_DAYS_PER_MONTH; day++)
+            while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < WORK_DAYS_PER_MONTH)
             {
+                totalWorkingDays++;
                 Random random = new Random();
                 int empCheck = random.Next(0, 3);
                 switch (empCheck)
@@ -30,11 +32,11 @@ namespace EmployeeWageCalculations
                         empHrs = 0;
                         break;
                 }
-                empWage = empHrs * EMP_RATE_PER_HOUR;
-                totalEmpWage += empWage;
-                Console.WriteLine("Daily Employee Wage is:" + empWage);
+                totalEmpHrs += empHrs;
+                Console.WriteLine("Day:  " + totalWorkingDays + "  Employee Working Hours: " + empHrs);
             }
-            Console.WriteLine("Monthly Employee Wage is :" + totalEmpWage);
+            int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine("Total Employee Wage is:" + totalEmpWage);     
         }
     }
 
