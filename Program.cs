c6a67b5f7676b8cce39b23c3620f98e9b3c00608
shwetagavai933 <shwetagavai933@gmail.com>
2021-09-05 17:00:57 +0530

    [Shweta]: Calculating Monthly Employee Wage

--- Program.cs
+++ Program.cs
@@ -7,28 +7,36 @@ namespace EmployeeWageCalculations
         public const int IS_PRESENT_FULL_TIME = 1;
         public const int IS_PRESENT_HALF_TIME = 2;
         public const int EMP_RATE_PER_HOUR = 20;
+        public const int WORK_DAYS_PER_MONTH = 10;
         static void Main(string[] args)
         {
             int empHrs = 0;
             int empWage = 0;
-            
-            Random random = new Random();
-            int empCheck = random.Next(0, 2);
-            switch (empCheck)
+            int totalEmpWage = 0;
+
+            for (int day = 0; day < WORK_DAYS_PER_MONTH; day++)
             {
-                case IS_PRESENT_FULL_TIME:
-                    empHrs = 8;
-                    break;
-                case IS_PRESENT_HALF_TIME:
-                    empHrs = 4;
-                    break;
-                default:
-                    empHrs = 0;
-                    break;
+                Random random = new Random();
+                int empCheck = random.Next(0, 3);
+                switch (empCheck)
+                {
+                    case IS_PRESENT_FULL_TIME:
+                        empHrs = 8;
+                        break;
+                    case IS_PRESENT_HALF_TIME:
+                        empHrs = 4;
+                        break;
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+                empWage = empHrs * EMP_RATE_PER_HOUR;
+                totalEmpWage += empWage;
+                Console.WriteLine("Daily Employee Wage is:" + empWage);
             }
-            
-            empWage = empHrs * EMP_RATE_PER_HOUR;
-            Console.WriteLine("Daily Employee Wage is:" + empWage);
+            Console.WriteLine("Monthly Employee Wage is :" + totalEmpWage);
         }
     }
+
+
 }
