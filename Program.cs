d65c44d6cbf1ee85d85c510a69cdc312e88c2bed
shwetagavai933 <shwetagavai933@gmail.com>
2021-09-05 15:56:54 +0530

    [Shweta]: Calculated Part Time Employee Wage

--- Program.cs
+++ Program.cs
@@ -7,19 +7,24 @@ namespace EmployeeWageCalculations
         static void Main(string[] args)
         {
             int IS_PRESENT_FULL_TIME = 1;
+            int IS_PRESENT_HALF_TIME = 2;
             int EMP_RATE_PER_HOUR = 20;
 
             int empHrs = 0;
             int empWage = 0;
+            
             Random random = new Random();
-
             int empCheck = random.Next(0, 2);
 
             if (empCheck == IS_PRESENT_FULL_TIME)
             {
                 empHrs = 8;
             }
-            else
+            else if (empCheck == IS_PRESENT_HALF_TIME)
+            {
+                empHrs = 4;
+            }
+            else 
             {
                 empHrs = 0;
             }
